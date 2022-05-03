23166a68dffc4961157642221021d3e3006a08dd
Jones-bass <jonesbass.tb@gmail.com>
2022-05-03 19:57:17 -0300

    Criando campos do veiculo

--- script.ts
+++ script.ts
@@ -1,7 +1,39 @@
+interface Veiculo {
+  nome: string;
+  placa: string;
+  entrada: Date;
+}
 
 (function() {
   const $ = (query: string): HTMLInputElement | null => 
   document.querySelector(query);
+
+  function patio() {
+    function ler() {}
+
+    function adicionar(veiculo: Veiculo) {
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+      <td>${veiculo.nome}</td>
+      <td>${veiculo.placa}</td>
+      <td>${veiculo.entrada}</td>
+      <td>
+      <button class="delete" data-placa="${veiculo.placa}">X</button>
+      </td>
+      `;
+
+      $("#patio")?.appendChild(row);
+    }
+
+    function remover() {}
+
+    function salvar() {}
+
+    function render() {}
+
+    return {ler, adicionar, remover, salvar, render };
+  }
 
   $("#cadastrar")?.addEventListener("click", () => {
     const nome = $("#nome")?.value;
@@ -11,5 +43,6 @@
       alert("Os campos nome e placa são obrigatórios");
       return;
     }
+    patio().adicionar({nome, placa, entrada: new Date()})
   })
 })();
